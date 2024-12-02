80d2a26507978dfc1f38316e78127fb2d2b50108
n455im07 <n455im07@gmail.com>
2024-12-02 17:17:15 +0100

    verif bash

--- c-wire.sh
+++ c-wire.sh
@@ -6,18 +6,37 @@ if  [ $# -eq 0 ]; then
     exit 1
 fi
 
+# Check if file_csv is a file and is readable
 file_csv=$1
-
-# Check if file_csv is a file
 if [ ! -f $file_csv ]; then
     echo "Error : File not found."
     exit 1
 fi
+if [ ! -r $file_csv ]; then
+    echo "Error : File unreadable."
+    exit 1
+fi
 
 # Station type
+station_type=$2
+if [ "$station_type" != "hvb" ] && [ "$station_type" != "hva" ] && [ "$station_type" != "lv" ]; then
+    echo "Error : Station type not valid."
+    exit 1
+fi
 
 # Consumer type
+consumer_type=$3
+if [ "$consumer_type" != "comp" ] && [ "$consumer_type" != "indiv" ] && [ "$consumer_type" != "all" ]; then
+    echo "Error : Consumer type not valid."
+    exit 1
+fi
+if [ "$consumer_type" != "comp" ] && [ $station_type != "lv" ] ; then
+    echo "Error : Forbidden option, only comp are linked to HV-B and HV-A stations."
+    exit 1
+fi
 
-# Power plant id
+# Power plant id (optional)
+power_plant_id=$4
 
-# Help option
+# Help option (optional)
+# ?bool ? (-h)
