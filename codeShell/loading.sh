ef1b3c8aa6ff23eae55e11c394a096f1c458d099
nassim jamhour <e-njamhour@prc-cy216-008.pedagogie.cyu.fr>
2024-12-20 14:01:30 +0100

    error audio

--- codeShell/loading.sh
+++ codeShell/loading.sh
@@ -2,23 +2,6 @@
 printf '\033[1;32;32m'
 speed=0.1
 
-# Défine the audio file 
-audio_file="/mnt/data/CY-Bunker-master/CY-Bunker-master/codeShell/Stuka Dive Sound.mp3"  
-
-# Vérifying if the audio file existe and if its not lisible
-if [ ! -f "$audio_file" ]; then
-    echo "Error : the audio file is not existing or not lisible"
-    exit 1
-else
-    echo "audio file found: $audio_file"
-fi
-
-# begin the audio
-mpv --no-video --loop "$audio_file" &  
-
-# recover the PID to kill the sound after
-audio_pid=$!
-
 p="    ╔════════════════════════════════════════════════════════════════╗
     ║                                                                ║
     ║                       ⛤          ⛤                             ║
