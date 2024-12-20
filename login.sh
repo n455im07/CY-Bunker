a46a963d9e0c2717dedd9d63e7ee15b217d48514
HichamEjh <166216207+HichamEjh@users.noreply.github.com>
2024-12-20 15:32:33 +0100

    Add files via upload

---
+++ login.sh
@@ -0,0 +1,233 @@
+#!/bin/bash
+printf '\033[1;32;32m'
+speed=0.3
+
+p1="    ╔════════════════════════════════════════════════════════════════╗
+    ║                                                                ║
+    ║                                                                ║
+    ║                                                                ║
+    ║                                                                ║
+    ║                                                                ║
+    ║                                                                ║
+    ║                                                                ║
+    ║                                                                ║
+                                                                      
+  ██████╗██╗   ██╗     ██████╗ ██╗   ██╗███╗   ██╗██╗  ██╗███████╗██████╗ 
+ ██╔════╝╚██╗ ██╔╝     ██╔══██╗██║   ██║████╗  ██║██║ ██╔╝██╔════╝██╔══██╗
+ ██║      ╚████╔╝█████╗██████╔╝██║   ██║██╔██╗ ██║█████╔╝ █████╗  ██████╔╝
+ ██║       ╚██╔╝ ╚════╝██╔══██╗██║   ██║██║╚██╗██║██╔═██╗ ██╔══╝  ██╔══██╗
+ ╚██████╗   ██║        ██████╔╝╚██████╔╝██║ ╚████║██║  ██╗███████╗██║  ██║
+  ╚═════╝   ╚═╝        ╚═════╝  ╚═════╝ ╚═╝  ╚═══╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝
+    ║                                                                ║
+    ║    ✘ Управление бункером®                                      ║
+    ║                                                                ║
+    ║                                                                ║
+    ║                 ┏━━━━━━━━━━━━━━━━━━━━━━━━━┓                    ║\n"
+
+
+
+p2="    ║                 ┗━━━━━━━━━━━━━━━━━━━━━━━━━┛                    ║  
+    ║                                                                ║ 
+    ║                 ┏━━━━━━━━━━━━━━━━━━━━━━━━━┓                    ║
+    ║        Password ┃                         ┃                    ║
+    ║                 ┗━━━━━━━━━━━━━━━━━━━━━━━━━┛                    ║
+    ║                                                                ║
+    ║                                                                ║
+    ║                                                                ║
+    ║                                                                ║
+    ╚════════════════════════════════════════════════════════════════╝"
+
+printf "$p1" 
+printf "    ║        Username ┃█                        ┃                    ║\n"
+printf "$p2" 
+sleep $speed
+clear
+##
+printf "$p1" 
+printf "    ║        Username ┃I█                       ┃                    ║\n"
+printf "$p2" 
+sleep $speed
+clear
+##
+printf "$p1" 
+printf "    ║        Username ┃IA█                      ┃                    ║\n"
+printf "$p2" 
+sleep $speed
+clear
+##
+printf "$p1" 
+printf "    ║        Username ┃IA-█                     ┃                    ║\n"
+printf "$p2" 
+sleep $speed
+clear
+##
+printf "$p1" 
+printf "    ║        Username ┃IA-n█                    ┃                    ║\n"
+printf "$p2" 
+sleep $speed
+clear
+##
+printf "$p1" 
+printf "    ║        Username ┃IA-ns█                   ┃                    ║\n"
+printf "$p2" 
+sleep $speed
+clear
+##
+printf "$p1" 
+printf "    ║        Username ┃IA-nse█                  ┃                    ║\n"
+printf "$p2" 
+sleep $speed
+clear
+##
+printf "$p1" 
+printf "    ║        Username ┃IA-nser█                 ┃                    ║\n"
+printf "$p2" 
+sleep $speed
+clear
+##
+printf "$p1" 
+printf "    ║        Username ┃IA-nserm█                ┃                    ║\n"
+printf "$p2" 
+sleep $speed
+clear
+##
+printf "$p1" 
+printf "    ║        Username ┃IA-nsermi█               ┃                    ║\n"
+printf "$p2" 
+sleep $speed
+clear
+##
+printf "$p1" 
+printf "    ║        Username ┃IA-nsermin█              ┃                    ║\n"
+printf "$p2" 
+sleep $speed
+clear
+##
+printf "$p1" 
+printf "    ║        Username ┃IA-nsermin               ┃                    ║\n"
+printf "$p2" 
+sleep $speed
+clear
+##
+##P1
+##
+p1="    ╔════════════════════════════════════════════════════════════════╗
+    ║                                                                ║
+    ║                                                                ║
+    ║                                                                ║
+    ║                                                                ║
+    ║                                                                ║
+    ║                                                                ║
+    ║                                                                ║
+    ║                                                                ║
+                                                                      
+  ██████╗██╗   ██╗     ██████╗ ██╗   ██╗███╗   ██╗██╗  ██╗███████╗██████╗ 
+ ██╔════╝╚██╗ ██╔╝     ██╔══██╗██║   ██║████╗  ██║██║ ██╔╝██╔════╝██╔══██╗
+ ██║      ╚████╔╝█████╗██████╔╝██║   ██║██╔██╗ ██║█████╔╝ █████╗  ██████╔╝
+ ██║       ╚██╔╝ ╚════╝██╔══██╗██║   ██║██║╚██╗██║██╔═██╗ ██╔══╝  ██╔══██╗
+ ╚██████╗   ██║        ██████╔╝╚██████╔╝██║ ╚████║██║  ██╗███████╗██║  ██║
+  ╚═════╝   ╚═╝        ╚═════╝  ╚═════╝ ╚═╝  ╚═══╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝
+    ║                                                                ║
+    ║    ✘ Управление бункером®                                      ║
+    ║                                                                ║
+    ║                                                                ║
+    ║                 ┏━━━━━━━━━━━━━━━━━━━━━━━━━┓                    ║
+    ║        Username ┃IA-nsermin               ┃                    ║
+    ║                 ┗━━━━━━━━━━━━━━━━━━━━━━━━━┛                    ║
+    ║                                                                ║
+    ║                 ┏━━━━━━━━━━━━━━━━━━━━━━━━━┓                    ║\n"
+
+
+
+p2="    ║                 ┗━━━━━━━━━━━━━━━━━━━━━━━━━┛                    ║
+    ║                                                                ║
+    ║                                                                ║
+    ║                                                                ║
+    ║                                                                ║
+    ╚════════════════════════════════════════════════════════════════╝"
+
+printf "$p1" 
+printf "    ║        Password ┃█                        ┃                    ║\n"
+printf "$p2" 
+sleep 0.5
+clear
+##
+printf "$p1" 
+printf "    ║        Password ┃*█                       ┃                    ║\n"
+printf "$p2" 
+sleep $speed
+clear
+##
+printf "$p1" 
+printf "    ║        Password ┃**█                      ┃                    ║\n"
+printf "$p2" 
+sleep $speed
+clear
+##
+printf "$p1" 
+printf "    ║        Password ┃***█                     ┃                    ║\n"
+printf "$p2" 
+sleep $speed
+clear
+##
+printf "$p1" 
+printf "    ║        Password ┃****█                    ┃                    ║\n"
+printf "$p2" 
+sleep $speed
+clear
+##
+printf "$p1" 
+printf "    ║        Password ┃*****█                   ┃                    ║\n"
+printf "$p2" 
+sleep $speed
+clear
+##
+printf "$p1" 
+printf "    ║        Password ┃******█                  ┃                    ║\n"
+printf "$p2" 
+sleep $speed
+clear
+##
+printf "$p1" 
+printf "    ║        Password ┃*******█                 ┃                    ║\n"
+printf "$p2" 
+sleep $speed
+clear
+##
+printf "$p1" 
+printf "    ║        Password ┃********█                ┃                    ║\n"
+printf "$p2" 
+sleep $speed
+clear
+##
+printf "$p1" 
+printf "    ║        Password ┃*********█               ┃                    ║\n"
+printf "$p2" 
+sleep $speed
+clear
+##
+printf "$p1" 
+printf "    ║        Password ┃**********█              ┃                    ║\n"
+printf "$p2" 
+sleep $speed
+clear
+##
+printf "$p1" 
+printf "    ║        Password ┃***********█             ┃                    ║\n"
+printf "$p2" 
+sleep $speed
+clear
+##
+printf "$p1" 
+printf "    ║        Password ┃************█            ┃                    ║\n"
+printf "$p2" 
+sleep $speed
+clear
+##
+printf "$p1" 
+printf "    ║        Password ┃************             ┃                    ║\n"
+printf "$p2" 
+sleep 1
+clear
+printf '\033[0m'
+
